732108a2522de3ad702b8fca6a71df24fe844201
ChrisMBarrett <chrisbarrett@Chriss-MacBook-Pro-7.local>
2014-09-22 22:03:55 +1200

    changes 2

--- login/login-exec.php
+++ login/login-exec.php
@@ -79,7 +79,7 @@ else
 	//		$usertableupdate = mysql_query("Update tbl_users set logins=logins+1 where tbl_users_id = $userid");
 	//		$userloginsert = mysql_query("Insert into tbl_user_log (tenant_id,user_id,ip_address,last_login) 
 
-			header("location: ../dashboard/");
+			header("location: ../Dashboard/");
 			exit();
 }			
 ?>
