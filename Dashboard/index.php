1ec1ddf0f271557ee1924f983a7f130401050fb7
ChrisMBarrett <chrisbarrett@Chriss-MacBook-Pro-7.local>
2014-09-23 19:50:58 +1200

    Changes

--- Dashboard/index.php
+++ Dashboard/index.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
 <?php
-session_start();
-
 // Include Files
-include '../includes/DBConnect.inc';
+// Authorisation File
+include '../includes/auth.php';
+// Database Connection File
+include '../includes/dbconnect.inc';
 
 // Get Session Variables
 $UserName = $_SESSION['UserName'];
@@ -136,12 +136,12 @@ $rows_returned = $rs->num_rows;
                         <i class="fa fa-user fa-fw"></i>  <i class="fa fa-caret-down"></i>
                     </a>
                     <ul class="dropdown-menu dropdown-user">
-                        <li><a href="#"><i class="fa fa-user fa-fw"></i> <?php echo $UserName; ?> Profile</a>
+                        <li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
                         </li>
                         <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
                         </li>
                         <li class="divider"></li>
-                        <li><a href="login.html"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+                        <li><a href="../login/logout.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
                         </li>
                     </ul>
                     <!-- /.dropdown-user -->
